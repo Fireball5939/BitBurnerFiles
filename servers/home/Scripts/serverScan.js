d9d5c87fdc9f2cf6944a406b533584c38a03d9db
Fireball5939 <111195856+Fireball5939@users.noreply.github.com>
2024-06-25 19:13:55 -0500

    Undo the meme changes

--- servers/home/Scripts/serverScan.js
+++ servers/home/Scripts/serverScan.js
@@ -1,5 +1,5 @@
-/** @param {NS} iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll */
-export async function main(iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll) {
+/** @param {NS} ns */
+export async function main(ns) {
 // Declare variables
 
 // Declare strings
@@ -20,7 +20,7 @@ let currentServer = 0;
 let ramToUse = 100;
 
 // Declare arrays
-let serverList = iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.scan(`home`);
+let serverList = ns.scan(`home`);
 let scannedServers = [];
 let serverClasses = [];
 
@@ -42,33 +42,33 @@ class Server
 
     inform(type, destination) 
     {
-        if (destination = `terminal`) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.tprint(`Information about this server is: ${type}`);
-        else if (destination = `logs`) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.print(`Information about this server is: ${type}`);
+        if (destination = `terminal`) ns.tprint(`Information about this server is: ${type}`);
+        else if (destination = `logs`) ns.print(`Information about this server is: ${type}`);
         else console.log(`Information about this server is: ${type}`);
     }
 }
 
 // Check to see if we have a given port buster
-if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.fileExists(`BruteSSH.exe`, `home`)) hasSSH = true;
-if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.fileExists(`FTPCrack.exe`, `home`)) hasFTP = true;
-if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.fileExists(`relaySMTP.exe`, `home`)) hasSMTP = true;
-if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.fileExists(`HTTPWorm.exe`, `home`)) hasHTTP = true;
-if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.fileExists(`SQLInject.exe`, `home`)) hasSQL = true;
+if (ns.fileExists(`BruteSSH.exe`, `home`)) hasSSH = true;
+if (ns.fileExists(`FTPCrack.exe`, `home`)) hasFTP = true;
+if (ns.fileExists(`relaySMTP.exe`, `home`)) hasSMTP = true;
+if (ns.fileExists(`HTTPWorm.exe`, `home`)) hasHTTP = true;
+if (ns.fileExists(`SQLInject.exe`, `home`)) hasSQL = true;
 
 // If the -c or --copyfile argument is given then give a prompt for the player to give a file to copy
-if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`-c`) || iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`--copyfile`)) copyFile = await iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.prompt(`What file are we copying?`, { type: `text` });
+if (ns.args.includes(`-c`) || ns.args.includes(`--copyfile`)) copyFile = await ns.prompt(`What file are we copying?`, { type: `text` });
 
 // If the --runfile argument is given then give a prompt for the player to give a file to run
-if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`--runfile`)) {
-    runFile = await iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.prompt(`What file are we running?`, { type: `text` });
-    ramToUse = await iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.prompt(`How much ram do we use with this? Range: 0-100 (Percent value)`, { type: `text` });
+if (ns.args.includes(`--runfile`)) {
+    runFile = await ns.prompt(`What file are we running?`, { type: `text` });
+    ramToUse = await ns.prompt(`How much ram do we use with this? Range: 0-100 (Percent value)`, { type: `text` });
 }
 
 // Iterate over every server we find, executing functions if they are given.
 for (; currentServer < serverList.length; currentServer++) 
     {
     // Get the current server being scanned as an object
-    const targetServer = iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.getServer(serverList[currentServer]);
+    const targetServer = ns.getServer(serverList[currentServer]);
 
     // Skip the server if it has already been scanned or is home
     if (scannedServers.includes(targetServer.hostname) || targetServer.hostname.includes(`home`)) continue;
@@ -77,7 +77,7 @@ for (; currentServer < serverList.length; currentServer++)
     scannedServers.push(targetServer.hostname);
 
     // Create a buffer list of every server connected to this one
-    const buffer = iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.scan(targetServer.hostname);
+    const buffer = ns.scan(targetServer.hostname);
 
     // Filter out all servers that have already been scanned from the buffer list
     buffer.filter((server) => !server.includes(scannedServers));
@@ -99,27 +99,27 @@ for (; currentServer < serverList.length; currentServer++)
     )
 
     // If --root is a given argument then gain root access on the server
-    if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`--root`) && !targetServer.hasAdminRights) 
+    if (ns.args.includes(`--root`) && !targetServer.hasAdminRights) 
     {
-        if (!targetServer.sshPortOpen && hasSSH) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.brutessh(targetServer.hostname);
-        if (!targetServer.ftpPortOpen && hasFTP) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.ftpcrack(targetServer.hostname);
-        if (!targetServer.smtpPortOpen && hasSMTP) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.relaysmtp(targetServer.hostname);
-        if (!targetServer.httpPortOpen && hasHTTP) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.httpworm(targetServer.hostname);
-        if (!targetServer.sqlPortOpen && hasSQL) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.sqlinject(targetServer.hostname);
-        try { iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.nuke(targetServer.hostname) } catch { }
+        if (!targetServer.sshPortOpen && hasSSH) ns.brutessh(targetServer.hostname);
+        if (!targetServer.ftpPortOpen && hasFTP) ns.ftpcrack(targetServer.hostname);
+        if (!targetServer.smtpPortOpen && hasSMTP) ns.relaysmtp(targetServer.hostname);
+        if (!targetServer.httpPortOpen && hasHTTP) ns.httpworm(targetServer.hostname);
+        if (!targetServer.sqlPortOpen && hasSQL) ns.sqlinject(targetServer.hostname);
+        try { ns.nuke(targetServer.hostname) } catch { }
     }
 
     // If -k or --killall is a given argument then kill all running scripts on the server
-    if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`-k`) || iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`--killall`)) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.killall(targetServer.hostname);
+    if (ns.args.includes(`-k`) || ns.args.includes(`--killall`)) ns.killall(targetServer.hostname);
 
     // If -c or --copyfile is a given argument then copy a given file to the server
-    if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`-c`) || iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`--copyfile`)) iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.scp(copyFile, targetServer.hostname);
+    if (ns.args.includes(`-c`) || ns.args.includes(`--copyfile`)) ns.scp(copyFile, targetServer.hostname);
 
     // If --runfile is a given argument then run a given file on the server IF it can be run
-    if (iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.args.includes(`--runfile`) && iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.fileExists(runFile, targetServer.hostname) && (runFile.includes(`.js`) || runFile.includes(`.scripts`))) 
+    if (ns.args.includes(`--runfile`) && ns.fileExists(runFile, targetServer.hostname) && (runFile.includes(`.js`) || runFile.includes(`.scripts`))) 
     {
-        const threads = Math.floor((targetServer.maxRam - targetServer.ramUsed) * ramToUse / iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.getScriptRam(runFile, targetServer.hostname));
-        iJustReallyWantToPissOffXsinxWithThisCodeForNoReasonAtAll.exec(runFile, targetServer.hostname, threads);
+        const threads = Math.floor((targetServer.maxRam - targetServer.ramUsed) * ramToUse / ns.getScriptRam(runFile, targetServer.hostname));
+        ns.exec(runFile, targetServer.hostname, threads);
     }
 }
 }
